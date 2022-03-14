b6e1ad13cdeebef4511f69cc913808d9e2a694f6
Lý Ngọc Hữu <huuln9@gmail.com>
2022-03-14 14:18:52 +0700

    update: securit network

--- lib/main.dart
+++ lib/main.dart
@@ -501,30 +501,31 @@ class _QRViewExampleState extends State<QRViewExample> {
   Future<void> _connectWifi(splitted, ssidIndex) async {
     // find password & security index
     int passwordIndex = 0;
-    int securityIndex = 0;
+    // int securityIndex = 0;
     for (var i = 0; i < splitted.length; i++) {
       if (splitted[i].toLowerCase() == 'p') {
         passwordIndex = i + 1;
-      } else if (splitted[i].toLowerCase() == 't') {
+      } /*else if (splitted[i].toLowerCase() == 't') {
         securityIndex = i + 1;
-      }
+      }*/
     }
     // NetworkSecurity networkSecurity;
-    String networkSecurity;
-    switch (splitted[securityIndex]) {
-      case 'WPA':
-        // networkSecurity = NetworkSecurity.WPA;
-        networkSecurity = 'WPA';
-        break;
-      case 'WEP':
-        // networkSecurity = NetworkSecurity.WEP;
-        networkSecurity = 'WEP';
-        break;
-      default:
-        // networkSecurity = NetworkSecurity.NONE;
-        networkSecurity = 'NONE';
-        break;
-    }
+    // String networkSecurity;
+    // switch (splitted[securityIndex]) {
+    //   case 'WPA':
+    //     networkSecurity = NetworkSecurity.WPA;
+    //     networkSecurity = 'WPA';
+    //     break;
+    //   case 'WEP':
+    //     networkSecurity = NetworkSecurity.WEP;
+    //     networkSecurity = 'WEP';
+    //     break;
+    //   default:
+    //     networkSecurity = NetworkSecurity.NONE;
+    //     networkSecurity = 'NONE';
+    //     break;
+    // }
+
     // WiFiForIoTPlugin.connect(
     //   splitted[ssidIndex],
     //   password: splitted[passwordIndex],
@@ -534,14 +535,13 @@ class _QRViewExampleState extends State<QRViewExample> {
     // );
 
     if (Platform.isAndroid) {
-      _connectWifiInDeviceAndroid(
-          splitted[ssidIndex], splitted[passwordIndex], networkSecurity);
+      _connectWifiInDeviceAndroid(splitted[ssidIndex], splitted[passwordIndex]);
     } else if (Platform.isIOS) {
       _connectWifiInDeviceIos();
     }
   }
 
-  _connectWifiInDeviceAndroid(ssid, password, securitty) async {
+  _connectWifiInDeviceAndroid(ssid, password) async {
     final arguments = {'ssid': ssid, 'password': password};
     final String result =
         await wifiAndroidChannel.invokeMethod('connectWifiInDevice', arguments);
