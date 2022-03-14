9019c3010cb458982e50b82c32fbec413fefdd07
Lý Ngọc Hữu <huuln9@gmail.com>
2022-03-14 14:52:53 +0700

    swift connect wifi

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -10,4 +10,17 @@ import Flutter
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
+
+  var configuration = NEHotspotConfiguration.init(ssid: "wifi name", passphrase: "wifi password", isWEP: false)
+  configuration.joinOnce = true
+
+  NEHotspotConfigurationManager.shared.apply(configuration) { (error) in
+      if error != nil {
+          //an error occurred
+          print(error?.localizedDescription)
+      }
+      else {
+          //success
+      }
+  }
 }
